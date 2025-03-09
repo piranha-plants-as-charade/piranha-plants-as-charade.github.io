f5c1cbea1f6c6635fcfdcbb77032f0e4bf4e2ce3
Max Huang <letmaxknow@gmail.com>
2025-03-09 12:12:50 -0400

    blog overhaul

---
+++ src/components/Blog.tsx
@@ -0,0 +1,15 @@
+import styles from "@/styles/Blog.module.css";
+
+const Blog = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Behind &ldquo;Piranha Plants as Charade&rdquo;</h2>
+        <h3>Results</h3>
+        <h3>How it Works</h3>
+      </div>
+    </>
+  );
+};
+
+export { Blog };
